269d1bcebf94ff25d5da73bb344b6c79d435630a
Léo Teissier <100595766+acsioses@users.noreply.github.com>
2022-04-11 16:05:52 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+const sr = ScrollReveal({
+    origin: 'left',
+    distance: '60px',
+    duration: 3000,
+    reset: true
+});
+sr.reveal('h2',{
+
+})
+sr.reveal('p',{
+
+})
+
+const src = ScrollReveal({
+    origin: 'top',
+    distance: '20px',
+    duration: 3000,
+    reset: true
+})
+src.reveal('nav',{
+
+})
+sr.reveal('h1',{
+
+})
